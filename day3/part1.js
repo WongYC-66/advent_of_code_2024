4ad4ea1de49c7bcdb727d7c12b7b8a52a6a42986
WongYC-66 <yengchien@hotmal.com>
2024-12-05 19:22:44 +0800

    add solution day 3

---
+++ day3/part1.js
@@ -0,0 +1,39 @@
+// https://adventofcode.com/2024/day/1
+const fs = require('node:fs/promises');
+
+const readFile = async (fileName) => {
+    try {
+        const data = await fs.readFile(`./${fileName}`, { encoding: 'utf8' });
+        // console.log(data);
+        return data
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+const getSum = (str) => {
+    let regex = /mul\((\d{1,3}),(\d{1,3})\)/g
+    let captures = [...str.matchAll(regex)]
+    return captures
+        .map(capture => Number(capture[1] * capture[2]))
+        .reduce((s, x) => s + x, 0)
+}
+
+
+const main = async () => {
+    // let rawFile = await readFile("sample1.txt")
+    let rawFile = await readFile("input.txt")
+    console.log(rawFile)
+    rawFile = rawFile
+        .replaceAll("\r", "")
+        .split("\n")
+    console.log(rawFile)    
+
+    let res = rawFile.reduce((sum, str) => getSum(str) + sum, 0)
+    console.log(res)
+    return res
+
+}
+
+
+main()
---
+++ day3/part2.js
@@ -0,0 +1,55 @@
+// https://adventofcode.com/2024/day/1
+const fs = require('node:fs/promises');
+
+const readFile = async (fileName) => {
+    try {
+        const data = await fs.readFile(`./${fileName}`, { encoding: 'utf8' });
+        // console.log(data);
+        return data
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+const getSum = (str) => {
+    let regex = /(mul\((\d{1,3}),(\d{1,3})\))|do\(\)|don't\(\)/g
+    let numRegex = /\d{1,3}/g
+    let captures = [...str.match(regex)]
+
+    console.log(captures)
+    let stack = []
+    let enabled = true
+    for (let str of captures) {
+        if (str == 'do()') {
+            enabled = true
+        } else if (str == "don't()") {
+            enabled = false
+        } else {
+            // 
+            if(!enabled) continue
+            let [n1, n2] = str.match(numRegex)
+            stack.push(Number(n1) * Number(n2))
+        }
+    }
+    return stack.reduce((s, x) => s + x, 0)
+}
+
+
+const main = async () => {
+    // let rawFile = await readFile("sample2.txt")
+    let rawFile = await readFile("input.txt")
+    console.log(rawFile)
+    rawFile = rawFile
+        .replaceAll("\r", "")
+        .split("\n")
+        .join("")
+    console.log(rawFile)
+
+    let res = getSum(rawFile)
+    console.log(res)
+    return res
+
+}
+
+
+main()
