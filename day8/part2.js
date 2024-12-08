94baa71639d25b21144f49500c333387f5a4234b
WongYC-66 <yengchien@hotmal.com>
2024-12-09 00:02:10 +0800

    add day 8 solution

---
+++ day8/part2.js
@@ -0,0 +1,99 @@
+// https://adventofcode.com/2024/day/8
+const fs = require('node:fs/promises');
+
+const readFile = async (fileName) => {
+    try {
+        const data = await fs.readFile(`./${fileName}`, { encoding: 'utf8' });
+        // console.log(data);
+        return data
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+
+const drawAndReturnCount = (arrOfStr) => {
+    let grid = arrOfStr.map(str => str.split(''))
+    let M = grid.length
+    let N = grid[0].length
+
+    const isValidCoords = (r, c) => {
+        return r >= 0 && r < M && c >= 0 && c < N
+    }
+
+    const canDraw = (r, c) => {
+        return grid[r][c] == '.'
+    }
+
+    const coordsToStr = (r, c) => `${r}-${c}`
+
+    let antennaCoords = {}  // { 'A' : [[0,1], ...] }
+
+    let uniqueLocAntenna = new Set()
+
+    for (let r = 0; r < M; r++) {
+        for (let c = 0; c < N; c++) {
+            let cell = grid[r][c]
+            if (cell == '.' || cell == '#') continue
+            // is digit/letter
+            // mark prev
+            antennaCoords[cell] && antennaCoords[cell].forEach(([r2, c2]) => {
+                let dr = r - r2
+                let dc = c - c2
+
+                // add antinode at currLoc
+                for (let i = 0; i < Infinity; i++) {
+                    let anti1_r = r + dr * i
+                    let anti1_c = c + dc * i
+                    if (isValidCoords(anti1_r, anti1_c)) {
+                        uniqueLocAntenna.add(coordsToStr(anti1_r, anti1_c))
+                        if (canDraw(anti1_r, anti1_c))
+                            grid[anti1_r][anti1_c] = '#'
+                    } else {
+                        break
+                    }
+                }
+
+                // add antinode at prevLoc
+                for (let i = 0; i < Infinity; i++) {
+                    let anti2_r = r2 - dr * i
+                    let anti2_c = c2 - dc * i
+                    if (isValidCoords(anti2_r, anti2_c)) {
+                        uniqueLocAntenna.add(coordsToStr(anti2_r, anti2_c))
+                        if (canDraw(anti2_r, anti2_c))
+                            grid[anti2_r][anti2_c] = '#'
+                    } else {
+                        break
+                    }
+                }
+            })
+
+
+            // add curr coords to hashmap
+            if (!antennaCoords[cell]) antennaCoords[cell] = []
+            antennaCoords[cell].push([r, c])
+        }
+    }
+
+    grid = grid.map(arr => arr.join(''))
+    console.log(grid)
+
+    return uniqueLocAntenna.size
+}
+
+const main = async () => {
+    // let rawFile = await readFile("sample.txt")
+    let rawFile = await readFile("input.txt")
+    rawFile = rawFile
+        .replaceAll("\r", "")
+        .split("\n")
+
+    console.log(rawFile)
+    let res = drawAndReturnCount(rawFile)
+    console.log(res)
+
+    return res
+}
+
+
+main()
