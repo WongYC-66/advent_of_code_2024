16bade670d7676b792d48466050d40cefe2fb4c3
WongYC-66 <yengchien@hotmal.com>
2024-12-09 02:44:39 +0800

    refactor: day3

--- day3/part1.js
+++ day3/part1.js
@@ -1,38 +1,31 @@
 // https://adventofcode.com/2024/day/3
-const fs = require('node:fs/promises');
+const { readFile } = require("../lib.js")
 
-const readFile = async (fileName) => {
-    try {
-        const data = await fs.readFile(`./${fileName}`, { encoding: 'utf8' });
-        // console.log(data);
-        return data
-    } catch (err) {
-        console.log(err);
-    }
-}
-
-const getSum = (str) => {
+const getLineSum = (str) => {
     let regex = /mul\((\d{1,3}),(\d{1,3})\)/g
     let captures = [...str.matchAll(regex)]
-    return captures
-        .map(capture => Number(capture[1] * capture[2]))
-        .reduce((s, x) => s + x, 0)
+    let productArr = captures.map(capture => Number(capture[1] * capture[2]))
+    return sum(productArr)
 }
 
+const sum = arr => {
+    return arr.reduce((s, x) => s + x, 0)
+}
 
 const main = async () => {
     // let rawFile = await readFile("sample1.txt")
     let rawFile = await readFile("input.txt")
-    console.log(rawFile)
     rawFile = rawFile
         .replaceAll("\r", "")
         .split("\n")
-    console.log(rawFile)    
+    // console.log(rawFile)    
 
-    let res = rawFile.reduce((sum, str) => getSum(str) + sum, 0)
+    // let res = rawFile.reduce((sum, str) => getSum(str) + sum, 0)
+    let res = sum(rawFile.map(getLineSum))
     console.log(res)
     return res
-
+    // expected sample.txt = 161
+    // expected input.txt = 181345830
 }
 
 
--- day3/part2.js
+++ day3/part2.js
@@ -1,24 +1,13 @@
 // https://adventofcode.com/2024/day/3
 
-const fs = require('node:fs/promises');
-
-const readFile = async (fileName) => {
-    try {
-        const data = await fs.readFile(`./${fileName}`, { encoding: 'utf8' });
-        // console.log(data);
-        return data
-    } catch (err) {
-        console.log(err);
-    }
-}
+const { readFile } = require("../lib.js")
 
 const getSum = (str) => {
     let regex = /(mul\((\d{1,3}),(\d{1,3})\))|do\(\)|don't\(\)/g
-    let numRegex = /\d{1,3}/g
     let captures = [...str.match(regex)]
 
     console.log(captures)
-    let stack = []
+    let products = []
     let enabled = true
     for (let str of captures) {
         if (str == 'do()') {
@@ -26,30 +15,37 @@ const getSum = (str) => {
         } else if (str == "don't()") {
             enabled = false
         } else {
-            // 
             if(!enabled) continue
-            let [n1, n2] = str.match(numRegex)
-            stack.push(Number(n1) * Number(n2))
+            products.push(getProduct(str))
         }
     }
-    return stack.reduce((s, x) => s + x, 0)
+    return sum(products)
 }
 
+const getProduct = (str) => {
+    let numRegex = /\d{1,3}/g
+    let [n1, n2] = str.match(numRegex).map(Number)
+    return n1 * n2
+}
+
+const sum = arr => {
+    return arr.reduce((s, x) => s + x, 0)
+}
 
 const main = async () => {
     // let rawFile = await readFile("sample2.txt")
     let rawFile = await readFile("input.txt")
-    console.log(rawFile)
     rawFile = rawFile
         .replaceAll("\r", "")
-        .split("\n")
-        .join("")
-    console.log(rawFile)
+        // .split("\n")
+
+    // console.log(rawFile)
 
     let res = getSum(rawFile)
     console.log(res)
     return res
-
+    // expected sample.txt = 48
+    // expected input.txt = 98729041
 }
 
 
